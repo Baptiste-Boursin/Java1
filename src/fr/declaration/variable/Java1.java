07e1510ad8c1b90c838dcdeb2e91740d41feb9ee
Baptiste-Boursin <baptiste.boursin@epsi.fr>
2020-10-27 14:08:45 +0100

    First commit

--- src/fr/declaration/variable/Java1.java
+++ src/fr/declaration/variable/Java1.java
@@ -2,6 +2,6 @@ package fr.declaration.variable;
 public class Java1 {
     public static void main(String[] args){ //main
         byte b = 10;
-        System.out.println("Hello world !\n byte= "+ b); // sout
+        System.out.println("Hello world !\n byte= "+ b ); // sout
     }
 }
